7a4625e21dfe0e76422fbe49d474a5eec6330d00
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-04-18 20:10:42 +0400

    added game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,6 +12,7 @@ import {
 import Card from '../components/Card';
 import Input from '../components/Input';
 import Colors from '../constants/colors';
+import NumberContainer from '../components/NumberContainer';
 
 const StartGameScreen = (props) => {
 	const [enteredValue, setEnteredValue] = useState('');
@@ -48,12 +49,19 @@ const StartGameScreen = (props) => {
 		setConfirmed(true);
 		setSelectedNumer(chosenNumber);
 		setEnteredValue('');
+		Keyboard.dismiss();
 	};
 
 	let confirmedOutput;
 
 	if (confirmed) {
-		confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+		confirmedOutput = (
+			<Card style={styles.summaryContainer}>
+				<Text>You selected</Text>
+				<NumberContainer>{selectedNumber}</NumberContainer>
+				<Button title='START GAME' />
+			</Card>
+		);
 	}
 
 	return (
@@ -127,6 +135,10 @@ const styles = StyleSheet.create({
 	input: {
 		width: 50,
 		textAlign: 'center',
+	},
+	summaryContainer: {
+		marginTop: 20,
+		alignItems: 'center',
 	},
 });
 

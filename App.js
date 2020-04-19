2ab7f4ed7ef3da09b8718fd2cc6784117820fc45
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-04-19 14:04:57 +0400

    finished game logic

--- App.js
+++ App.js
@@ -10,26 +10,33 @@ export default function App() {
 	const [userNumber, setUserNumber] = useState(); // entered number by user
 	const [guessRounds, setGuessRounds] = useState(0); //rounds
 
+	const configureNewGameHandler = () => {
+		setGuessRounds(0);
+		setUserNumber(null);
+	};
+
 	const startGameHandler = (selectedNumber) => {
 		setUserNumber(selectedNumber);
-		setGuessRounds(0);
 	};
 
 	const gameOverHandler = (numOfRounds) => {
 		setGuessRounds(numOfRounds);
 	};
 
-	let content = (
-		<StartGameScreen
-			onStartGame={startGameHandler}
-			onGameOver={gameOverHandler}
-		/>
-	);
+	let content = <StartGameScreen onStartGame={startGameHandler} />;
 
 	if (userNumber && guessRounds <= 0) {
-		content = <GameScreen userChoice={userNumber} />;
+		content = (
+			<GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+		);
 	} else if (guessRounds > 0) {
-		content = <GameOverScreen />;
+		content = (
+			<GameOverScreen
+				roundsNumber={guessRounds}
+				userNumber={userNumber}
+				onRestart={configureNewGameHandler}
+			/>
+		);
 	}
 
 	return (
